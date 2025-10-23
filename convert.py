533105b31fb66dbe7426d70f5bd7ae03fc4042d7
daxtonhs <daxton.brouillette@student.doralacademynv.org>
2025-10-23 11:40:50 -0700

    p

--- convert.py
+++ convert.py
@@ -2,16 +2,20 @@ import os
 
 path = "/home/datd-tech/Data/3d Models"
 
+skipped = 0
+
 folders = [os.path.join(path, f) for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
 
-folder = folders[1]
+for folder in folders:
 
-print(folder)
+    print(folder)
 
-allowed_ext = ["jpg", "png"]
+    allowed_ext = ["jpg", "png"]
 
-images = [image for image in os.listdir(folder) if image.split(".")[-1] in allowed_ext]
+    images = [image for image in os.listdir(folder) if image.split(".")[-1] in allowed_ext]
 
-thumbnail = max((os.path.join(folder, f) for f in images), key=os.path.getsize)
-
-print(thumbnail)
+    if(len(images) > 0):
+        thumbnail = max((os.path.join(folder, f) for f in images), key=os.path.getsize)
+        print(thumbnail)
+    else:
+        skipped += 1
