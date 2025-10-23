45784aba34e63fcaacc3f55f2ca15f061e0b9b1b
daxtonhs <daxton.brouillette@student.doralacademynv.org>
2025-10-23 11:34:35 -0700

    uhhhhhh

--- convert.py
+++ convert.py
@@ -1,8 +1,15 @@
 import os
 
-path = "/home/datd-tech/Data"
+path = "/home/datd-tech/Data/3d Models"
 
 folders = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
 
-for folder in folders:
-    print(folder)
+folder = folders[0]
+
+allowed_ext = ["jpg", "png"]
+
+images = [image for image in os.listdir(folder) if image.split(".")[-1] in allowed_ext]
+
+thumbnail = max((os.path.join(folder, f) for f in images), key=os.path.getsize)
+
+print(thumbnail)
