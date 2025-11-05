cfcf1bc3218ab5a1bcfd918d6d4cca892c3e7ec5
daxtonhs <daxton.brouillette@student.doralacademynv.org>
2025-11-05 12:23:56 -0800

    ggg

--- findthefiles.py
+++ findthefiles.py
@@ -14,6 +14,9 @@ conn = mariadb.connect(
 
 cur = conn.cursor()
 
+os.makedirs("thumbnails", exist_ok=True)
+
+
 contents = os.listdir(lookpath)
 for item in contents:
     print(item)
@@ -30,24 +33,61 @@ for item in contents:
         else:
             continue
 
-        tags = {
-            "3D Model": [".fbx", ".obj", ".stl", ".glb"],
-            "Normal": ["normal"],
-            "Ambient Occlusion": ["ao", "ambient"],
-            "Metallic": ["metallic"],
+        fileCheckTags = {
+            "Model": [".fbx", ".obj", ".stl", ".glb"],
+            "Texture": ["texture","tex","t_map","map","diffuse","albedo","basecolor","normal","n_map","nm","bump","bump_map","roughness","r_map","gloss","gloss_map","metallic","metal","metal_map","specular","spec_map","emissive","emission","emit","self_illum","selfillum","height","hmap","displacement","disp","alpha","a_map","opacity","mask","ao","ambient_occlusion","occlusion"],
+            "Normal": ["normal","n_map","normal_map","nm","nrm","bump","bump_map","tangent","tangent_space"],
+            "Ambient Occlusion": ["ambient_occlusion","ao","ao_map","occlusion","ao_mask","ambient_occl","ambient_occl_map"],
+            "Roughness": ["roughness","r_map","rough","roughness_map","rough_map","gloss","glossiness","gloss_map"],
+            "Metallic": ["metallic","metalness","m_map","metal","metalness_map","metal_map","specular","spec_map"],
+            "Alpha": ["alpha","a_map","opacity","mask_alpha","transparency","trans","opacity_map","cutout","mask","clip"],
+            "Specular": ["spec", "specular", "gloss", "rough", "reflect", "metal"],
+            "Emission": ["emissive","emission","emit","self_illum","selfillum","self_emission","em","e_map","em_map","illum","illumination","lightmap_emission","mask_emissive"],
+            "Height": ["height","hmap","displacement","disp","d_map","heightmap","height_map","displace","elevation","zmap"],
+            "Displacement": ["displacement","disp","d_map","displace","disp_map","displacement_map","subdiv","subdivision","tessellation","tess_map"],
+            "Audio": [".mp3", ".wav", ".ogg", ".m4a"],
+            ".obj": [".obj"],
+            ".fbx": [".fbx"],
+            ".gltf": [".gltf"],
+            ".blend": [".blend"],
+            ".ma": [".ma"],
+            ".max": [".max"],
+            ".usdz": [".usdz"],
+            ".3ds": [".3ds"],
+            ".dae": [".dae"],
+            ".png": [".png"],
+            ".jpg": [".jpg"],
+            ".bmp": [".bmp"],
+            ".hdr": [".hdr"],
+            ".exr": [".exr"],
+            ".pfm": [".pfm"],
+            "HDRI": ["hdri","env","environment","envmap","env_map","sky","skybox","lighting","light_probe","lightprobe","panorama","panoramic","360","spherical","exr","hdr"]
         }
 
         tagsList = ["Asset"]
 
         for file in files:
-            for tag in tags:
-                for check in tags[tag]:
+            for tag in  fileCheckTags:
+                for check in  fileCheckTags[tag]:
                     if(check in os.path.basename(file).lower() and tag not in tagsList):
                         tagsList.append(tag)
 
         tags = ','.join(tagsList)
+
+        #Minor post processing
+        if("Model" in tags and "Texture" in tags):
+            """Removing the texture tag from the object if it contains
+            3D models as the textures always relate to the 3D model and
+            will cause confusion when being marked as an object & texture
+            """
+            tags.remove("Texture") 
+
+
+        
+
         title = assetName
-        imagePath = thumbnail if thumbnail else ""
+        imagePath = thumbnail
+
         assetPath = fullpath
 
         print(tags, title, imagePath, assetPath)
